0e7894a483774ce1fadbf8f77d31794c33a84dbc
Shivani9698 <119753029+Shivani9698@users.noreply.github.com>
2024-02-22 18:07:51 +0530

    Update DHT11.ino

--- DHT11.ino
+++ DHT11.ino
@@ -1,6 +1,6 @@
 #include "DHT.h"
 
-#define DHTPIN 26     // what pin we're connected to
+#define DHTPIN 2     // what pin we're connected to
 
 // Uncomment whatever type you're using!
 #define DHTTYPE DHT11   // DHT 11 
